e41238ee40678d59e54a588aed5fa18c8399a897
jacky <jackywu526@gmail.com>
2024-08-27 01:55:06 +0800

    feat(skill and tools svg): 新增 技術工具區 的對應 svg 圖片

---
+++ src/assets/skillandtools/index.ts
@@ -0,0 +1,23 @@
+import elementplus from './elementplus.svg';
+import eslint from './eslint.svg';
+import git from './git.svg';
+import js from './js.svg';
+import ts from './ts.svg';
+import vite from './vite.svg';
+import jira from './jira.svg';
+import vue from './vue.svg';
+import scss from './scss.svg';
+import tailwindcss from './tailwindcss.svg';
+
+export const svgs = {
+  elementplus,
+  eslint,
+  git,
+  js,
+  ts,
+  vite,
+  jira,
+  vue,
+  scss,
+  tailwindcss
+};
