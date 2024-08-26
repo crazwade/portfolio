43d92f35a5856babbddc6bdb776333f766b48366
jacky <jackywu526@gmail.com>
2024-08-27 02:07:37 +0800

    feat(Contain): 新增 頁內容的共用容器組件

---
+++ src/components/Contain.vue
@@ -0,0 +1,18 @@
+<script setup lang='ts'>
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <div class="text-4xl border-borderColor border-b-[3px] pb-2">
+      {{ title }}
+    </div>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
