2cd88955228f1d8bf9f1ac7a4ce3ca6611e92cd0
jacky <jackywu526@gmail.com>
2024-08-27 20:56:39 +0800

    feat(Tag): 製作 Tag 組件

---
+++ src/components/Tag.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+defineProps<{
+  label: string;
+}>();
+</script>
+
+<template>
+  <div class="border-borderColor border-[1px] w-fit rounded-full py-1 px-3 text-borderColor">
+    {{ label }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
