5a904fedf9b061fb9ec3c3e203a38e0b2c73cf0b
jacky <jackywu526@gmail.com>
2024-08-29 12:10:50 +0800

    feat(HomeView): 新增 copyright 註解

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import AboutMe from './HomeView/AboutMe.vue';
+import Autobiography from './HomeView/Autobiography.vue';
 import SkillsAndTools from './HomeView/SkillsAndTools.vue';
 import SideProject from './HomeView/SideProject.vue';
 import Resume from './HomeView/Resume.vue';
+import ContactMe from './HomeView/ContactMe.vue';
 </script>
 
 <template>
   <div class="flex flex-col gap-16">
     <AboutMe />
+    <Autobiography />
     <SkillsAndTools />
     <SideProject />
     <Resume />
-    <div id="test5" class="h-[500px] w-full ">
-      聯繫我
+    <ContactMe />
+    <div class="h-[100px] w-full flex justify-center items-end pb-5">
+      Copyright 2024 Jacky Wu
     </div>
   </div>
 </template>
