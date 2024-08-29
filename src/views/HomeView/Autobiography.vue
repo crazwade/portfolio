0d76f83417ebf58a636d63c09442a3bd906ed457
jacky <jackywu526@gmail.com>
2024-08-29 12:12:06 +0800

    feat(Autobiography): 製作 自傳 頁面

---
+++ src/views/HomeView/Autobiography.vue
@@ -0,0 +1,29 @@
+<script setup lang='ts'>
+import Contain from '../../components/Contain.vue';
+</script>
+
+<template>
+  <div id="Autobiography" class="px-10">
+    <Contain
+      title="自傳"
+    >
+      <div class="flex flex-col justify-center items-center text-lg">
+        我是吳國揚 (Jacky Wu)，2019 年畢業於國立高雄大學資訊管理學系。高中時期曾赴美留學一年，大學期間曾轉學從義守大學至國立高雄大學，豐富的求學經歷培養了我獨立思考、自我檢討的習慣。
+        <br><br>
+        大學期間熱衷於實作課程，並積極參與實作專案，特別選修六角學院院長的校內課程，大四期間有幸能負責開發校內國際處招生系統，從中累積了全端開發經驗，並對前端開發產生興趣。
+        <br><br>
+        畢業後，我持續投入前端開發領域，目前已累積三年工作經驗。專精於Vue框架，負責系統頁面設計、撰寫前端規格文件、使用者互動設計以及與後端API串接。同時，積極學習新技術，熟練掌握 TypeScript、Vue3、Eslint 等工具，並能運用這些技術優化開發流程，提升開發效率。
+        <br><br>
+        未來，我希望能成為一名優秀的工程師，逐步實現以下目標：
+        <br><br>
+        1-5 年： 成為能獨立解決問題、快速學習的工程師。<br>
+        5-8 年： 成為具備領導能力，能帶領團隊完成專案的技術主管。<br>
+        8 年後： 成為技術專家，導入新技術、提升團隊效能。
+      </div>
+    </Contain>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
