3c602ba520c1794efdf812384b41e53b78352c05
jacky <jackywu526@gmail.com>
2024-08-30 15:22:23 +0800

    feat(Resume): 調整 履歷頁面呈現方式，添加如果超出兩頁的履歷有攤開效果

--- src/views/HomeView/Resume.vue
+++ src/views/HomeView/Resume.vue
@@ -1,5 +1,5 @@
 <script setup lang='ts'>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import Contain from '../../components/Contain.vue';
 import pdf1 from '../../assets/resume/pdf1.png';
 import pdf1e from '../../assets/resume/pdf1-e.png';
@@ -9,21 +9,37 @@ import { useI18n } from 'vue-i18n';
 
 const { t, locale } = useI18n();
 
-const resume = [
+const resume = ref([
   pdf1, pdf2
-];
+]);
 
-const changeimg = (action: 'next' | 'previous') => {
-
-  currentIndex.value = action === 'next' ? currentIndex.value + 1 : currentIndex.value - 1;
-
-  if (currentIndex.value < 0) {
-    currentIndex.value = resume.length - 1;
+watch(() => locale.value, () => {
+  if (locale.value === 'en-US') {
+    resume.value = [
+      pdf1e, pdf2e
+    ];
   }
 
-  if (currentIndex.value >= resume.length) {
-    currentIndex.value = 0;
+  if (locale.value  === 'zh-TW') {
+    resume.value = [
+      pdf1, pdf2
+    ];
   }
+});
+
+const downloadPDFs = () => {
+  resume.value.forEach((pdfUrl, index) => {
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = `jackywu-resume-page${index + 1}.png`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  });
+};
+
+const hrefTo = (url: string) => {
+  window.open(url, '_blank');
 };
 </script>
 
@@ -33,30 +49,24 @@ const changeimg = (action: 'next' | 'previous') => {
       :title="t('menu.Resume')"
     >
       <div class="flex flex-col gap-3 w-full px-6">
-        <div class="relative w-full h-full flex justify-center">
-          <button
-            class="absolute top-1/2 -left-5 -translate-y-1/2 w-[50px] h-[50px] opacity-80 hover:opacity-95 bg-borderColor rounded-full flex justify-center items-center"
-            @click="changeimg('previous')"
-          >
-            <i class="pi pi-chevron-left" />
-          </button>
-          <button
-            class="absolute top-1/2 -right-5 -translate-y-1/2 w-[50px] h-[50px] opacity-80 hover:opacity-95 bg-borderColor rounded-full flex justify-center items-center"
-            @click="changeimg('next')"
-          >
-            <i class="pi pi-chevron-right" />
-          </button>
-          <img :src="resume[currentIndex]" alt="" srcset="" class="w-full h-auto object-contain max-w-[600px] border-[8px] border-borderColor">
+        <div class="relative w-full h-full hidden justify-center md:flex">
+          <img :src="resume[1]" alt="" srcset="" class="w-full h-auto md:w-auto md:h-[700px] object-contain border-[8px] border-borderColor">
+          <img :src="resume[0]" alt="" srcset="" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-auto md:h-[700px] object-contain border-[8px] border-borderColor resumeTran">
+        </div>
+        <div class="relative w-full h-full flex justify-center md:hidden">
+          <img :src="resume[0]" alt="" srcset="" class="w-full h-auto md:w-auto md:h-[700px] object-contain border-[8px] border-borderColor">
         </div>
         <div class="flex flex-wrap gap-2 justify-center items-center">
           <div
             class="flex flex-row justify-between items-center p-2 text-backgroundColor bg-fontColor gap-2 cursor-pointer hover:opacity-80 select-none"
+            @click="downloadPDFs"
           >
             <i class="pi pi-download"></i>
             {{ t('下載PDF') }}
           </div>
           <div
             class="flex flex-row justify-between items-center p-2 text-fontColor bg-backgroundColor border-[1px] border-fontColor gap-2 cursor-pointer hover:opacity-80 select-none"
+            @click="hrefTo('https://crazwade.github.io/profile/')"
           >
             <i class="pi pi-eye"></i>
             {{ t('查看') }}
@@ -68,4 +78,8 @@ const changeimg = (action: 'next' | 'previous') => {
 </template>
 
 <style lang="scss" scoped>
+.resumeTran {
+  transform-origin: center;
+  transform: scale(0.95) translateX(-235px) translateY(-390px) rotateZ(10deg);
+}
 </style>
