8e29ab4798edf2b7fcfc8089c28f554cd9d00c25
jacky <jackywu526@gmail.com>
2024-08-30 15:21:38 +0800

    feat(ContactMe): 引入 i18n 並調整資料顯示方式

--- src/views/HomeView/ContactMe.vue
+++ src/views/HomeView/ContactMe.vue
@@ -1,43 +1,13 @@
 <script setup lang='ts'>
 import Contain from '../../components/Contain.vue';
 import CircleItem from '../../components/CircleItem.vue';
+import { useI18n } from 'vue-i18n';
+import { useDataStore } from '../../stores/data';
 
-type ItemType = {
-  type: 'icon';
-  icon: string;
-  url: string;
-} | {
-  type: 'mail';
-  icon: string;
-  mail: string;
-} | {
-  type: 'text';
-  text: string;
-  url: string;
-};
+const dataStore = useDataStore();
 
-const socials: ItemType[] = [
-  {
-    type: 'icon',
-    icon: 'pi pi-github',
-    url: 'https://github.com/crazwade',
-  },
-  {
-    type: 'icon',
-    icon: 'pi pi-linkedin',
-    url: 'https://www.linkedin.com/in/jackywu/',
-  },
-  {
-    type: 'text',
-    text: 'CV',
-    url: 'https://www.google.com',
-  },
-  {
-    type: 'mail',
-    icon: 'pi pi-envelope',
-    mail: 'jackywu526@gmail.com',
-  }
-];
+const { t } = useI18n();
+
 </script>
 
 <template>
@@ -47,13 +17,14 @@ const socials: ItemType[] = [
     >
       <div class="flex flex-col justify-center items-center">
         <div class="flex flex-row gap-3">
-          <template v-for="(item, index) in socials" :key="index">
+          <template v-for="(item, index) in dataStore.getSocials" :key="index">
             <CircleItem
               :size="'normal'"
               :type="item.type"
               :icon="item.type !== 'text' ? item.icon : undefined"
               :text="item.type === 'text' ? item.text : undefined"
               :needPointer="true"
+              @click="dataStore.handlehref(item)"
             />
           </template>
         </div>
