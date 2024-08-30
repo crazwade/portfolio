cb3ac8463041eeb163541a7eb27e94ab6032e543
jacky <jackywu526@gmail.com>
2024-08-30 15:22:59 +0800

    feat(Header): 新增 i18n 中英文調整按鈕區 並將菜單區統一由 dataStore 處理避免後續修改不易

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -8,33 +8,7 @@ import type { Lang } from '../lang';
 const { locale, t } = useI18n();
 
 const modeStroe = useModeStore();
-
-const menu: { label: string, id: string }[] = [
-  {
-    label: '首頁',
-    id: 'Aboutme',
-  },
-  {
-    label: '自傳',
-    id: 'Autobiography',
-  },
-  {
-    label: '技能樹',
-    id: 'SkillsAndTools',
-  },
-  {
-    label: '專案作品',
-    id: 'SideProject',
-  },
-  {
-    label: '履歷',
-    id: 'Resume',
-  },
-  {
-    label: '聯繫我',
-    id: 'ContactMe',
-  },
-];
+const dataStore = useDataStore();
 
 defineProps<{
   isOpen: boolean;
@@ -92,7 +66,7 @@ const changeLang = (lang: Lang) => {
         class="fixed top-0 left-0 mt-[78px] border-b-[8px] border-x-[8px] bg-backgroundColor border-borderColor w-full box-border flex flex-col"
       >
         <div class="flex flex-col justify-center items-center gap-3 my-2">
-          <template v-for="(item, index) in menu" :key="index">
+          <template v-for="(item, index) in dataStore.getMenu" :key="index">
             <a
               @click="scrollTo(item.id)"
               class="text-xl font-extrabold mt-[5px] w-full text-center hover:opacity-50 cursor-pointer"
@@ -103,8 +77,20 @@ const changeLang = (lang: Lang) => {
         </div>
         <div class="flex justify-between items-center mb-3 mt-8 mx-5">
           <!-- 語言設定 -->
-          <div>
-            i18n not ready yet...
+          <div class="flex flex-wrap justify-end">
+            <template v-for="(lang, key) in langName" :key="key">
+              <button
+                @click="changeLang(lang)"
+                v-if="lang === locale"
+                class="bg-borderColor px-2"
+              >
+                {{ lang }}
+              </button>
+              <button @click="changeLang(lang)" v-else>{{ lang }}</button>
+              <div v-if="key !== langName.length - 1" class="mx-1">
+                /
+              </div>
+            </template>
           </div>
           <!-- 主題設定 -->
           <div class="flex flex-wrap justify-end">
