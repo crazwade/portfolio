686b954b951fbba22ad1b729f6e315296289f3af
jacky <jackywu526@gmail.com>
2024-08-30 15:33:40 +0800

    feat(App): 顯示 version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router';
 import Layout from './layout/Layout.vue';
+import { version } from '../package.json';
+
+onMounted(() => {
+  console.log(`ver ${version}`);
+});
 </script>
 
 <template>
