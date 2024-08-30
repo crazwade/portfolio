0600f20df674286044f8af166997f125bb1ef578
jacky <jackywu526@gmail.com>
2024-08-30 17:07:14 +0800

    feat(AboutMe): 在個人頁面中引用 畢業學校

--- src/views/HomeView/AboutMe.vue
+++ src/views/HomeView/AboutMe.vue
@@ -9,17 +9,18 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <div id="Aboutme" class="flex justify-center items-center flex-col py-5 px-10 lg:px-40 xl:px-80">
+  <div id="Aboutme" class="flex justify-center items-center flex-col py-5 px-10 lg:px-20 xl:px-44">
     <div class="h-full flex flex-col md:flex-row md:items-center">
       <div class="h-full flex flex-col gap-5">
         <p class="text-8xl md:whitespace-nowrap">{{ t('aboutme.name')}}</p>
         <p class="text-4xl">{{ t('aboutme.title') }}</p>
+        <p class="text-base">{{ t('aboutme.school') }}</p>
       </div>
-      <span class="text-xl mt-5 md:mt-0 md:pl-10">
+      <span class="text-2xl mt-5 md:mt-0 md:pl-10 text-borderColor">
         {{ t('aboutme.info') }}
       </span>
     </div>
-    <div class="w-full flex justify-start mt-5 gap-3">
+    <div class="w-fit flex justify-start mt-5 gap-3">
       <template v-for="(item, index) in dataStore.getSocials" :key="index">
         <CircleItem
           :size="'normal'"
