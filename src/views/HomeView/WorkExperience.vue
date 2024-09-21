c69ec95798d5c5f11b2dc174a1e3dfc337da31ed
jacky <jackywu526@gmail.com>
2024-09-21 23:27:50 +0800

    feat(WorkExperience): 工作經歷的主元件，引用工作經驗共用組件

---
+++ src/views/HomeView/WorkExperience.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+import { useDataStore } from '../../stores/data';
+import WorkExp from '../../components/WorkExp.vue';
+
+const dataStore = useDataStore();
+
+</script>
+
+<template>
+  <div id="WorkExperience" class="h-fit flex flex-shrink-0 justify-center items-center flex-col py-5 px-2 sm:px-10 lg:px-20 xl:px-30">
+    <WorkExp :data="dataStore.getWorks"/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
